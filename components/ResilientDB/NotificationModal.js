8a1e855cd5e70c3bc12b4735a93c6645478c10c1
unknown <amy.anvu@gmail.com>
2024-12-07 19:07:19 -0800

    Changed notification modal.

--- components/ResilientDB/NotificationModal.js
+++ components/ResilientDB/NotificationModal.js
@@ -3,14 +3,14 @@ import { Modal, Button } from 'react-bootstrap';
 
 const NotificationModal = ({ show, title, message, onClose }) => {
   return (
-    <Modal show={show} onHide={onClose} centered className="custom-modal">
-      <Modal.Header closeButton className="custom-modal-header">
+    <Modal show={show} onHide={onClose} centered className="bg-blueGray-700 text-white flex flex-col items-center justify-center absolute w-auto h-auto">
+      <Modal.Header closeButton className="text-md uppercase font-bold px-2 py-2">
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
-      <Modal.Body className="custom-modal-body">
+      <Modal.Body className="px-2 py-2 text-sm text-white">
         <p>{message}</p>
       </Modal.Body>
-      <Modal.Footer className="custom-modal-footer">
+      <Modal.Footer className="px-2 px-2 bg-white text-blueGray-400 rounded">
         <Button variant="primary" onClick={onClose}>
           OK
         </Button>
